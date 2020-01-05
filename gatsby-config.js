75556a97785ec56ea0f01500d6101e38441161a8
Thomas Pucci <thomas.lb.pucci@gmail.com>
2020-01-05 19:11:07 +0100

    :heavy_plus_sign: (gatsby-plugin-typescript) add dep

--- gatsby-config.js
+++ gatsby-config.js
@@ -27,8 +27,6 @@ module.exports = {
         icon: `src/images/gatsby-icon.png`,
       },
     },
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
+    `gatsby-plugin-typescript`,
   ],
 }
